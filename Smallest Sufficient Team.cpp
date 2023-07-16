e5f8fe4d1a8aa85845a03e0ac20628ffdb43d168
Atharva More <88966976+moreatharvaa@users.noreply.github.com>
2023-07-16 21:50:28 +0530

    Create Smallest Sufficient Team.cpp

---
+++ Smallest Sufficient Team.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+
+        int skills  = req_skills.size();
+        int nopeople = people.size();
+        int possible = pow(2, skills);
+
+        unordered_map<string, int> mapping;
+        for(int i=0; i<skills; i++){
+            mapping[req_skills[i]] = i;
+        }
+
+
+        unordered_map<int, vector<int>> dp;
+        dp.reserve(1 <<skills);
+        dp[0] = {};
+
+        for(int i=0; i<nopeople;i++){
+            int size = people[i].size();
+            int val = 0;
+            for(int j=0;j<size;j++){
+                int index = mapping[people[i][j]];
+                val = val | (1<<index);
+            }
+
+
+            for(auto it: dp){
+                int comb = it.first | val;
+                if(dp.find(comb) == dp.end() || dp[comb].size() > 1 + dp[it.first].size()){
+                    dp[comb] = it.second;
+                    dp[comb].push_back(i);
+                }
+            }
+        }
+
+        return dp[possible -1];
+
+        
+        
+    }
+};
