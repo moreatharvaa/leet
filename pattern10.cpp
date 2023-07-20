569b662d81091fe75fe324440be4c4bd7c308505
Atharva More <88966976+moreatharvaa@users.noreply.github.com>
2023-07-20 21:00:47 +0530

    Create pattern10.cpp

---
+++ pattern10.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        for(int i=0; i<n;i++){
+            for(int j =0;j<i+1;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        for(int i=1; i<n;i++){
+            for(int j =0;j<n-i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        
+        
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
