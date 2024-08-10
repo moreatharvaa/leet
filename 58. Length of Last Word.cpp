ca4a209f3f574f5af1959112689a7f82eaa8e9a5
Atharva More <88966976+moreatharvaa@users.noreply.github.com>
2024-08-10 19:45:05 +0530

    Update 58. Length of Last Word.cpp

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -17,3 +17,22 @@ public:
         return length;
     }
 };
+
+
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        
+        s.erase(s.find_last_not_of(' ') + 1); //Removes the trailing spaces
+        
+        
+        int lastSpace = s.find_last_of(' '); // Find the last space character
+
+        
+        return lastSpace == string::npos ? s.length() : s.length() - lastSpace - 1; 
+        // Calculate the length of the last word
+        
+    }
+};
